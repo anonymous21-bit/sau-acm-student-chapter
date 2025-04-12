6a1175059dd565aff25c51a0717f03b95d757f60
mukulsharma15 <mlegend9070@gmail.com>
2025-04-12 22:28:24 +0530

    Fix GitHub Pages deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/sau-acm-student-chapter/',
   server: {
     port: 5173,
     host: true
