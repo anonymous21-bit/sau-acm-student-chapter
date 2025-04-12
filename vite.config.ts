1a01b387222709c901aab8d6cbaa85a2b70d9d01
mukulsharma15 <mlegend9070@gmail.com>
2025-04-12 22:54:24 +0530

    Fix asset loading issues for Netlify deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/sau-acm-student-chapter/',
+  base: './',
   server: {
     port: 5173,
-    host: true
-  }
+  },
 });
