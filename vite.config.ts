5bb36f8357a76db11bf372f21798f0fe2c1c858a
mukulsharma15 <mlegend9070@gmail.com>
2025-04-18 15:14:12 +0530

    netlify changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/sau-acm-student-chapter/',
+  base: process.env.NETLIFY === 'true' ? '/' : '/sau-acm-student-chapter/',
   server: {
     port: 5173,
   },
